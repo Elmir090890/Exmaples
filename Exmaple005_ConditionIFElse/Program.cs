7753c2288e324c98fed620e762335cf2f0624034
Elmir <elmir090890@mail.ru>
2023-01-26 01:53:02 +0300

    Condition IF Else

---
+++ Exmaple005_ConditionIFElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter the name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Hooray, is it MASHA");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
